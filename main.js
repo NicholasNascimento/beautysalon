16197f41db9b54687fd41902cad22608504d28ca
Nicholas <nicholas.an99@gmail.com>
2022-05-13 21:47:06 -0300

    Finishing third class

--- main.js
+++ main.js
@@ -6,3 +6,46 @@ for(const element of toggle) {
     nav.classList.toggle('show')
   })
 }
+
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
+  })
+}
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(`
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+`, { interval: 100 })
